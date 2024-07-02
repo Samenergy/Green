4d42e68484ae8d80a8271d3afe603220df5e782b
Samenergy <s.dushime@alustudent.com>
2024-07-02 12:30:13 +0200

    d

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className="max-w-full mx-auto bg-black">
         <footer className="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
           <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-            © 2022{" "}
+            © 2024{" "}
             <a
               href="https://flowbite.com"
               className="hover:underline"
